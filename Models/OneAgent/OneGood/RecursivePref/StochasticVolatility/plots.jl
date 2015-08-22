35e4bab91eccad2d3e22b7a56b552029e4ef2543
Spencer Lyon <spencerlyon2@gmail.com>
2015-08-22 10:03:31 -0400

    ENH: cleaned plot file a little bit

--- Models/OneAgent/OneGood/RecursivePref/StochasticVolatility/plots.jl
+++ Models/OneAgent/OneGood/RecursivePref/StochasticVolatility/plots.jl
@@ -6,6 +6,8 @@ using CompEcon
 # @load "BFZ_additive_k18-32.jld"
 @load "BFZ_additive_k18-32_rho-9.0_alpha-9.0.jld"
 
+#jld file should have m, c, vf, coefs, kp
+
 
 evaluate(coefs, k::Vector, x::Vector, v::Vector, order=[0 0 0]) =
     funeval(coefs, m.basis, [k x v], order)
@@ -41,30 +43,6 @@ function plot(coefs; trans=(x,y)->(x.^(m.agent.ρ-1).*y),
 
 
     fig[:savefig]("figures/rho_alpha_m9/plot$(fn_suffix).png")
-
-end
-
-
-function plot2()
-    grid, (kgrid, xgrid, vgrid) = nodes(m.basis)
-    fig, ax = subplots()
-
-    xbar = mean(xgrid); vbar = mean(vgrid)
-
-    J = eval_J(kgrid, xbar, vbar)
-    J_k = eval_J(kgrid, xbar, vbar)
-
-    y = log(J.^(m.agent.ρ - 1) .* J_k)
-    x = log(kgrid)
-
-    ax[:plot](x, y)
-    ax[:set_xlabel]("log(k)")
-    ax[:set_ylabel](L"log(J^{\rho - 1} J_k)")
-
-    approx_slope = (y[end] - y[1]) / (x[end] - x[1])
-    ax[:set_title]("slope: $approx_slope")
-
-    fig[:savefig]("figures/rho_alpham9/plot.png")
 
 end
 
