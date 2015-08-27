8d62b961233f55f3ae5c6d3908bfc7ecbbeab5e8
Spencer Lyon <spencerlyon2@gmail.com>
2015-08-27 15:12:54 -0400

    ENH? trying to automate buildling statespace

--- Models/ModelAbstraction.jl
+++ Models/ModelAbstraction.jl
@@ -31,18 +31,44 @@ abstract AbstractVariable
 abstract AbstractStateVariable <: AbstractVariable
 abstract AbstractJumpVariable <: AbstractVariable
 
-immutable EndogenousState{T} <: AbstractStateVariable
+immutable EndogenousState{T,BF,BP,TM} <: AbstractStateVariable
     grid::Vector{T}
-    # FOC::Function
-    # env_cond::Function
+    basis::Basis{1,BF,BP}
+    Φ::TM
     # name::Symbol  # could default to `gensym()` (which is julia for I don't care)
 end
 
-immutable ExogenousState{T} <: AbstractStateVariable
-    grid::Vector{T}  # grid for realizations in the current period
-    gridp::Matrix{T}  # grid combining all realizations today with all possible shocks
-    # transition::Function
+EndogenousState(grid::Vector) =
+    EndogenousState(grid, Basis(LinParams(grid, 0)))
+
+# Note depending on the type of Basis you pass, the grid inside the state
+# instance might be different than the grid you passed in.
+function EndogenousState(grid::Vector, b::Basis{1})
+    real_grid = nodes(b)[1]
+    Φ = CompEcon.evalbase(b.params[1], real_grid, 0)
+    EndogenousState(real_grid, b, Φ)
+end
+
+immutable ExogenousState{T,BF,BP,TM} <: AbstractStateVariable
+    grid::Vector{T}   # grid for realizations in the current period
+    gridp::Matrix{T}  # grid moving each i ∈ grid  with all possible shocks
+    basis::Basis{1,BF,BP}
+    Φ::TM
     # name::Symbol
+end
+
+ExogenousState{T}(grid::Vector{T}, gridp::Matrix{T}) =
+    ExogenousState(grid, gridp, Basis(LinParams(grid, 0)))
+
+function ExogenousState(grid::Vector, gridp, b::Basis{1})
+    real_grid = nodes(b)[1]
+    if real_grid != grid
+        msg = string("You must use `grid = nodes(b)` and compute `gridp`",
+                     " based on that grid or things won't line up")
+        throw(ArgumentError(msg))
+    end
+    Φ = CompEcon.evalbase(b.params[1], real_grid, 0)
+    ExogenousState(real_grid, gridp, b, Φ)
 end
 
 # ------------------------------------------------------------------- #
@@ -52,11 +78,44 @@ abstract AbstractExogenousProcess{N}
 
 include("MA_Exogenous.jl")
 
-
 # ------------------------------------------------------------------- #
 # State Spaces
 # ------------------------------------------------------------------- #
-abstract AbstractStateSpace{N}
+abstract AbstractStateSpace{Nendog,Nexog}
+
+immutable DefaultStateSpace{Nendog,Nexog,T,N} <: AbstractStateSpace{N}
+    enog::NTuple{Nendog,EndogenousState{T}}
+    exog::NTuple{Nexog,ExogenousState{T}}
+    grid::Matrix{T}
+    basis::Basis{N}
+end
+
+function call{N1,N2,T}(::Type{DefaultStateSpace{N1,N2,T}},
+                       endog::NTuple{N1,EndogenousState{T}},
+                       exog::NTuple{N2,ExogenousState{T}},
+                       grid::Matrix{T},
+                       b::Basis)
+    buildSS(endog, exog, (endog..., exog...), grid, b)
+end
+
+function buildSS{N1,N2,T,N}(endog::NTuple{N1,EndogenousState{T}},
+                            exog::NTuple{N2,ExogenousState{T}},
+                            states::NTuple{N,Union(EndogenousState{T}, ExogenousState{T})}
+                            grid::Matrix{T},
+                            b::Basis{N})
+    DefaultStateSpace{N1,N2,T,N}(endog, exog, grid, b)
+end
+
+function DefaultStateSpace{N1,N2,T}(endog::NTuple{N1,EndogenousState{T}},
+                                    exog::NTuple{N2,ExogenousState{T}})
+    grids = vcat(Vector[i.grid for i in endog], Vector[i.grid for i in exog])
+    grid = gridmake(grids...)
+    basis = Basis([i.basis for i in endog]..., [i.basis for i in exog]...)
+    DefaultStateSpace{N1,N2,T}(endog, exog, grid, basis)
+end
+
+# function DefaultStateSpace{N1,N2,T}(endog::NTuple{N1,Vector{T}},
+#                                     exog::NTuple{N2,Vector{T}})
 
 export AbstractEntity,
        AbstractConsumer, EpsteinZinAgent, utility, is_recursive,
@@ -65,6 +124,6 @@ export AbstractEntity,
        AbstractStateVariable, EndogenousState, ExogenousState,
        AbstractExogenousProcess,
        ConstantVolatility1, ConstantVolatility, StochasticVolatility, simulate,
-       AbstractStateSpace
+       AbstractStateSpace, DefaultStateSpace
 
 end
