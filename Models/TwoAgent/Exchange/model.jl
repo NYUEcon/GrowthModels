26bf99dcc016e39c1f6fd9ec9b7ecfb47bf5ec77
Spencer Lyon <spencerlyon2@gmail.com>
2015-09-23 14:35:47 -0400

    simulation algorithm fully fleshed out
    Now need to debug

--- Models/TwoAgent/Exchange/model.jl
+++ Models/TwoAgent/Exchange/model.jl
@@ -2,6 +2,9 @@ module Exchange
 
 using CompEcon
 
+include("util.jl")
+using .EDSUtil
+
 immutable Agent
     ρ::Float64
     β::Float64
@@ -16,9 +19,13 @@ end
 
 _unpack(a::Agent) = (a.ρ, a.β, a.α, a.η, a.ν, a.ω, a.σ, a.δ, a.ζ)
 
-function _agg(agent::Agent, a, b)
+# cobb douglass (σ = 0)
+# _agg(a::Agent, x, y) = x.^(1-a.ω) .* y.^(a.ω)
+
+# CES
+function _agg(agent::Agent, x, y)
     ω, σ = agent.ω, agent.σ
-    ((1-ω)*a.^σ + ω*b.^σ).^(1/σ)
+    ((1-ω)*x.^σ + ω*y.^σ).^(1/σ)
 end
 
 """
@@ -62,6 +69,8 @@ type BCFL22C
     agent1::Agent
     agent2::Agent
     γ::Float64
+    ϵ::Matrix{Float64}
+    Π::Vector{Float64}
 end
 
 # TODO: check the sigma parameters
@@ -73,10 +82,11 @@ function BCFL22C(;ρ1::Real=-1.0 , α1::Real=-9.0, β1::Real=0.999,  # EZ 1
                   η2::Real=1.0, ν2::Real=0.1,                     # production 2
                   σ2::Real=0.6, ω2::Real=0.7,                     # composite 2
                   δ2::Real=0.025, ζ2::Real=0.001,                 # other 2
-                  γ::Real=0.9)                                    # exog
+                  γ::Real=0.9, nϵ::Int=4)                         # exog
     agent1 = Agent(ρ1, β1, α1, η1, ν1, ω1, σ1, δ1, ζ1)
     agent2 = Agent(ρ2, β2, α2, η2, ν2, ω2, σ2, δ2, ζ2)
-    BCFL22C(agent1, agent2, γ)
+    ϵ, Π = EDSUtil.qnwgh(nϵ, 2)
+    BCFL22C(agent1, agent2, γ, ϵ, Π)
 end
 
 Base.writemime(io::IO, ::MIME"text/plain", m::BCFL22C) =
@@ -93,7 +103,7 @@ log z_{2,t+1} - log z_{1,t+1} = (2γ-1)(log z_{2,t}-log z_{1,t})
 ```
 
 So each increment is normally distributed with mean
-(2γ-1)(log z_{2,t} - log z_{1,t}) and variance v1 + v2
+(2γ-1)(log z_{2,t} - log z_{1,t}) and variance ζ1 + ζ2
 
 Arguments:
 
@@ -102,39 +112,27 @@ Arguments:
   that specifies that the simulation will run for `end - start` periods and
   every `thin` observations will be kept.
 """
-function simulate_exog(m::BCFL22C, keepers::StepRange{Int,Int}=1:1:10000,
-                       seed::Int=1)
+function simulate_exog(m::BCFL22C, capT::Int=10000, seed::Int=1)
     # simplify notation and set random seed
-    coef = (2*m.γ - 1)
-    ζ = m.agent1.ζ + m.agent2.ζ
+    coef_zbar = (2*m.γ - 1)
+    coef_g = (m.γ - 1)
+    ζ1, ζ2 = m.agent1.ζ, m.agent2.ζ
     srand(seed)
 
     # allocate memory and set initial state
-    N_out = length(keepers)
-    out = zeros(N_out)
-    out[1] = 0.0
-    step_length = step(keepers)
+    lzbar = zeros(capT)
+    lg = zeros(capT)
+    lzbar[1] = 0.0
     temp = 0.0
 
-    for keep=2:N_out
-        for _ in step_length
-            temp = coef*temp + ζ*randn()
-        end
-        out[keep] = temp
+    for t=2:capT
+        ϵ1, ϵ2 = randn(), randn()
+        old_lzbar = lzbar[t-1]
+
+        lzbar[t] = coef_zbar*old_lzbar + ζ1*ϵ1 - ζ2*ϵ2
+        lg[t] = coef_g*old_lzbar + ζ1*ϵ1
     end
-    out
-end
-
-function get_a_b_c(m::BCFL22C, sa, sb, zbar)
-    a1 = sa
-    a2 = 1-sa
-    b1 = sb*zbar
-    b2 = (1-sb)*zbar
-
-    c1 = _agg(m.agent1, a1, b1)
-    c2 = _agg(m.agent2, a2, b2)
-
-    return a1, a2, b1, b2, c1, c2
+    lzbar, lg
 end
 
 """
@@ -144,15 +142,13 @@ If we combine all 4 FOC and allow all params to be different, we get
 ((1-ω1)*(1-ω2))/(ω1*ω2) = (1-sa)^(σ2-1)/(sa^(σ1-1)) * (sb)^(σ1-1)/((1-sb)^(σ2-1))
 ```
 
-Given a guess for sa, we can solve the above for sb.
+Given a guess for sa = a1, we can solve the above for b2.
 
-Given sa, sb, zbar=z2/z1 we have
+Given a1, b2, zbar=z2/z1 we have
 
 ```
-a1 = sa
-a2 = 1-sa
-b1 = sb*zbar
-b2 = (1-sb)*zbar
+a2 = 1 - a1
+b1 = zbar - b2
 ```
 
 The above follows from the fact that in the exchange economy scaled by z1 we
@@ -163,135 +159,316 @@ Then we can compute c1, c2
 Given c1, c2 a1, a2 and stuff we can compute the residual
 
 ```
-♠ - (1-β2)*c2^(ρ2-σ2)*(1-ω2)*b2^(σ2-1)/((1-β1)*c1^(ρ1-σ1)*ω1*b1^(σ1-1))
+♠ - (c2/a2)^(1.0-σ2) * (c1/a1)^(σ1-1.0) * ω2/(1-ω1)
 ```
 
 or
 
 ```
-♠ - (1-β2)*c2^(ρ2-σ2)*ω2*a2^(σ2-1)/((1-β1)*c1^(ρ1-σ1)*(1-ω1)*a1^(σ1-1))
+♠ - (c2/b2)^(1.0-σ2) * (c1/b1)^(σ1-1.0) * (1-ω2)/ω1
 ```
 
-It isn't obvious which of the two residuals to use. In principle they should be
-the same at the solution.
-
+The use of either residual is accceptable. Given how we solved for b2, they are
+equivalent
 """
-function sa_resid(m::BCFL22C, ♠t, zbar, sa)
+function a1_resid(m::BCFL22C, ♠t, zbar, a1)
     ρ1, β1, α1, η1, ν1, ω1, σ1, δ1, ζ1 = _unpack(m.agent1)
     ρ2, β2, α2, η2, ν2, ω2, σ2, δ2, ζ2 = _unpack(m.agent2)
 
+    a2 = 1.0 - a1
+
     if abs(σ1 - σ2) < 1e-14  # sigmas are the same, used closed form sb
-        x = ((1-ω1)*(1-ω2)/(ω1*ω2))^(1/(σ1-1))*(sa/(1-sa))
-        sb = x / (1 + x)
+        y = ((1-ω1)*(1-ω2)/(ω1*ω2))^(1/(σ1-1))*(a1/a2)
+        b2 = zbar / (1 + y)
+
+        # cobb douglass
+        # x = (1-ω1)*(1-ω2)/(ω1*ω2)*(a2/a1)
+        # b2 = zbar*x/(1+x)
     else
         error("implement solver for sb")
     end
 
-    a1, a2, b1, b2, c1, c2 = get_a_b_c(m, sa, sb, zbar)
+    # get rest of allocation
+    b1 = zbar - b2
+    c1 = ((1-ω1)*a1^σ1 + ω1*b1^σ1)^(1/σ1)
+    c2 = ((1-ω2)*b2^σ2 + ω2*a2^σ2)^(1/σ2)
 
-    rhs = log(1-β2) + (ρ2-σ2) * log(c2) + log(ω2) + (σ2-1)*log(a2)
-    rhs += - (log(1-β1) + (ρ1-σ1)*log(c1) + log(1-ω1) + (σ1-1)*log(a1))
-    # rhs = (1-β2)*c2^(ρ2-σ2)*ω2*a2^(σ2-1)/((1-β1)*c1^(ρ1-σ1)*(1-ω1)*a1^(σ1-1))
-    return log(♠t) - rhs, (sb, a1, a2, b1, b2, c1, c2)
+    # compute RHS of residual for the a_1 FOC
+    rhs = (c2/a2)^(1.0-σ2) * (c1/a1)^(σ1-1.0) * ω2/(1-ω1)
+
+    # return the residual as first argument and allocation as second
+    ♠t - rhs, (a1, a2, b1, b2, c1, c2)
+end
+
+function get_allocation(m::BCFL22C, ♠, zbar)
+    a1 = brent(foo->a1_resid(m, ♠, zbar, foo)[1], 1e-15, 1-1e-15)
+
+    # evaluate once more to get the allocation at the optimal a1
+    a1, a2, b1, b2, c1, c2 = a1_resid(m, ♠, zbar, a1)[2]
+end
+
+function get_eds_grid(vectors::Vector...; δ::Float64=0.01, Mbar::Int=50)
+    capT = length(vectors[1])
+
+    sim_data = hcat(vectors...)
+    density = eval_density(sim_data, sim_data)[1]
+    inds = sortperm(density)
+    n_drop = ceil(Int, capT*0.01)
+    sort_data = sim_data[inds, :]
+    sort_data = sort_data[n_drop:end, :]
+
+    # now construct eds on this "thinned" out data
+    eds = eds_M(sort_data, Mbar)
+end
+
+# ------------------- #
+# ValueFunctions type #
+# ------------------- #
+immutable ValueFunctions
+    deg::Int
+    coefs::Matrix{Float64}  # first column U_1, second col U_2
+end
+
+# return a Vector of [U_1(♠, zbar), U_2(♠, zbar)]
+value_funcs(vfs::ValueFunctions, ♠::Real, zbar::Real) =
+    reshape(complete_polynomial([♠ zbar], vfs.deg) * vfs.coefs, 2)
+
+value_funcs(vfs::ValueFunctions, ♠::Vector, zbar::Vector) =
+    complete_polynomial([♠ zbar], vfs.deg) * vfs.coefs
+
+"""
+This function assumes you are passing the values of (♠, zbar) in period t.
+It will construct the implied (♠', zbar', g') for all integration nodes in
+m.ϵ and then hand off to the function below
+"""
+function certainty_equiv(m::BCFL22C, κ::Vector, vfs::ValueFunctions, ♠::Real,
+                         zbar::Real)
+    ♠p, zbarp, gp = get_next_grid(m, κ, ♠, zbar)
+    certainty_equiv(m, vfs, ♠p, zbarp, gp)
+end
+
+"""
+This method assumes you are passing (♠', zbar', g') for all the integration
+nodes in m.ϵ
+"""
+function certainty_equiv(m::BCFL22C, vfs::ValueFunctions, ♠p::Vector,
+                         zbarp::Vector, gp::Vector)
+    J = length(m.Π)
+    if J != length(♠p) || J != length(zbarp) || J != length(gp)
+        error("wrong dimensionality. ♠p, zbarp and gp should have $J elements")
+    end
+
+    # Compute value functions and evaluate expectations that form certainty
+    # equiv
+    VF = value_funcs(vfs, ♠p, zbarp)
+    μ1 = dot(m.Π, (gp.*VF[:, 1]).^(m.agent1.α))^(1.0/m.agent1.α)
+    μ2 = dot(m.Π, (gp.*VF[:, 2]).^(m.agent2.α))^(1.0/m.agent2.α)
+    μ1, μ2
+
+end
+
+# ----------------------- #
+# VFI for post simulation #
+# ----------------------- #
+
+function get_next_grid(m::BCFL22C, κ::Vector, ♠::Real, zbar::Real)
+    lzbar = log(zbar)
+    ϵ1, ϵ2 = m.ϵ[:, 1], m.ϵ[:, 2]
+
+    # get exog grid
+    lzbarp = (2.0*m.γ - 1.0)*lzbar + (m.agent1.ζ*ϵ1 - m.agent2.ζ*ϵ2)
+    lgp = (m.γ - 1.0)*lzbar + m.agent1.ζ*ϵ1
+
+    ♠p = κ[1] + κ[2]*♠ .+ κ[3]*lzbarp
+
+    return ♠p, exp(lzbarp), exp(lgp)
+end
+
+function get_next_grid(m::BCFL22C, κ::Vector, ♠::Vector, zbar::Vector)
+    lzbar = log(zbar)
+
+    ϵ1, ϵ2 = m.ϵ[:, 1], m.ϵ[:, 2]
+
+    # get exog grid
+    lzbarp = (2.0*m.γ - 1.0)*lzbar .+ (m.agent1.ζ*ϵ1 - m.agent2.ζ*ϵ2)'
+    lgp = (m.γ - 1.0)*lzbar .+ m.agent1.ζ*ϵ1'
+
+    ♠p = κ[1] + κ[2]*♠ .+ κ[3]*lzbarp
+
+    return ♠p, exp(lzbarp), exp(lgp)
+end
+
+# returns two functions that evaluate U_1(♠, zbar) and U_2(♠, zbar)
+function vfi_from_simulation(m::BCFL22C, κ::Vector, grid::Matrix{Float64};
+                             deg::Int=3, tol=1e-8, maxit::Int=5000)
+    # unpack
+    ρ1, β1, α1, η1, ν1, ω1, σ1, δ1, ζ1 = _unpack(m.agent1)
+    ρ2, β2, α2, η2, ν2, ω2, σ2, δ2, ζ2 = _unpack(m.agent2)
+    ♠grid, zbargrid = grid[:, 1], grid[:, 2]
+    Ngrid = length(♠grid)
+
+    # get consumption on the grid
+    cs = map((x,y)->get_allocation(m, x, y)[5:6], ♠grid, zbargrid)
+    c1 = map(x->getindex(x, 1), cs)
+    c2 = map(x->getindex(x, 2), cs)
+
+    # construct _constant_ basis matrix
+    basis_mat = complete_polynomial(grid, deg)
+
+    # get coefficients for degree deg complete poly in ♠ and zbar for the
+    # initial guess that U_i = c_i
+    coefs = basis_mat \ [c1 c2]
+    vfs = ValueFunctions(deg, coefs)
+
+    # advance grid one time step using integration nodes m.ϵ
+    ♠p, zbarp, gp = get_next_grid(m, κ, ♠grid, zbargrid)
+
+    # iteration manager stuff + allocate for value functions
+    err = 10.0
+    it = 0
+    U1_old, U2_old = copy(c1), copy(c2)
+    U1, U2 = similar(c1), similar(c2)
+
+    # TODO: this slices rows (yuck)
+    while err > tol && it < maxit
+        it += 1
+        for i=1:Ngrid
+            # compute certainty equivalents
+            μ1, μ2 = certainty_equiv(m, vfs, ♠p[i, :][:], zbarp[i, :][:],
+                                     gp[i, :][:])
+
+            # apply backward step to get U_{i,t}
+            U1[i] = ((1-β1)*c1[i]^ρ1 + β1*μ1^ρ1)^(1/ρ1)
+            U2[i] = ((1-β2)*c2[i]^ρ2 + β2*μ2^ρ1)^(1/ρ2)
+        end
+
+        # compute residual
+        err = max(maxabs(U1 - U1_old), maxabs(U1 - U1_old))
+
+        # update coefficient vector
+        coefs = basis_mat \ [U1 U2]
+        vfs = ValueFunctions(deg, coefs)
+
+        # update cache of previous U_i
+        copy!(U1_old, U1)
+        copy!(U2_old, U2)
+    end
+
+    vfs
 end
 
 
-function linear_coefs(m::BCFL22C, lzbar::Vector{Float64}=simulate_exog(m),
-                      b_old::Vector{Float64}=[0.05, 0.95, -0.75];
+function linear_coefs(m::BCFL22C, lzbar::Vector{Float64}=simulate_exog(m)[1],
+                      lg::Vector{Float64}=simulate_exog(m)[2],
+                      κ::Vector{Float64}=[0.05, 0.95, -0.75];
                       maxiter::Int=10_000, verbose::Bool=true,
                       ξ::Float64=0.1)
     # Make sure we are working with the endowment economy
-    if abs(m.agent1.η-1.0) > 1e-14 || abs(m.agent2.η - 1.0) > 1e-13
-        m = "Solution only implemented for endowment economy (η1=η2=1.0)"
-        error(m)
+    if abs(m.agent1.η-1.0) > 1e-14 || abs(m.agent2.η - 1.0) > 1e-14
+        msg = "Solution only implemented for endowment economy (η1=η2=1.0)"
+        error(msg)
     end
 
     # unpack
     ρ1, β1, α1, η1, ν1, ω1, σ1, δ1, ζ1 = _unpack(m.agent1)
     ρ2, β2, α2, η2, ν2, ω2, σ2, δ2, ζ2 = _unpack(m.agent2)
-    capT = length(lzbar)
+    capT = length(lzbar) - 1
+
+    # unpack exog
+    γ = m.γ
+    ϵ1 = m.ϵ[:, 1]
+    ϵ2 = m.ϵ[:, 2]
+    Π = m.Π
+    zbar = exp(lzbar)
+    g_all = exp(lg)
 
     # Iteration management stuff
     tol = 1e-4*ξ
     dist = 100.0
     it = 0
 
-    # ratio of pareto weights: agent 1/agent 2. Starts at 1.0. Is length T+1 so
-    # we can run the for loop below to fill in the entire X/RHS arrays. We will
-    # not use the last point of ♠
+    # ♠ is ratio multipliers: φ_1/φ_2. Starts at 1.0.
     ♠ = ones(capT+1)
-    X = [ones(capT) ♠[1:capT] lzbar]  # regression matrix
-    RHS = Array(Float64, capT)  # residual to use in regression
+    X = [ones(capT+1) ♠ lzbar]  # regression matrix
+    LHS = Array(Float64, capT-1)  # LHS of regression from euler equation
 
     ♠_old = fill(100.0, capT+1)
 
-    sa = Array(Float64, capT)  # residual to use in regression
-    sb = Array(Float64, capT)  # residual to use in regression
+    c1 = Array(Float64, capT)
+    c2 = Array(Float64, capT)
 
-    while dist > tol
+    while dist > tol && it < maxiter
 
-        showplot(plot(1:capT, ♠[1:capT]))
+        κ0, κ1, κ2 = κ
 
-        b0, b1, b2 = b_old
-        t = 1
-        # simulate ♠ forward and solve for sa along the way
-        @inbounds for t=1:capT
+        # simulate ♠ forward and solve for c1, c2 along the way. Need them so
+        # I can evaluate the euler equation later
+        for t=1:capT
             # extract time t state
             ♠t = ♠[t]
             lzt = lzbar[t]
-            zt = exp(lzt)
+            lztp = lzbar[t+1]
+            zbart = zbar[t]
 
-            # update ♠_{t+1}
-            ♠p = b0+ b1*♠t + b2*lzt
+            # update ♠_{t+1}. Is linear in ♠_t and log zbar_{t+1}
+            ♠p = κ0+ κ1*♠t + κ2*lztp
             ♠[t+1] = ♠p
 
-            # compute sa_t as a function of ♠_t and zbar_t
-            sa[t] = brent(foo->sa_resid(m, ♠t, zt, foo)[1], 1e-15, 1-1e-15)
+            # Solve for the optimal allocation at this ♠, zbart and store
+            # consumption in vectors
+            a1, a2, b1, b2, c1[t], c2[t] = get_allocation(m, ♠t, zbart)
 
-            # evaluate once more to get all the other intermediate variables
-            # at the sa optimum
-            sb[t], a1, a2, b1, b2, c1, c2 = sa_resid(m, ♠t, zt, sa[t])[2]
-
-            # fill in RHS (other residual we didn't use in sa_resid above)
-            # we don't need to do any transformations because the LHS is
-            # already supposed to represent ♠t
-            RHS_num = (1-β2)*c2^(ρ2-σ2)*(1-ω2)*b2^(σ2-1)
-            RHS_denom = (1-β1)*c1^(ρ1-σ1)*ω1*b1^(σ1-1)
-
-            # TODO: I think the problem must be in how I construct RHS.
-            #       study the Maliar examples more to confirm
-            RHS[t] = RHS_num / RHS_denom
-
-            # also fill in endog part of regression matrix for time t
+            # fill in this row of regresion matrix
             X[t, 2] = ♠t
         end
 
-        showplot(plot(1:capT, sa[1:capT]))
-        showplot(plot(1:capT, sb[1:capT]))
-        showplot(plot(1:capT, RHS[1:capT]))
-        showplot(plot(1:capT, ♠[1:capT]))
+        # build EDS grid
+        grid = get_eds_grid(♠, zbar)
 
-        # check convergence (computes `mean(abs(♠[1:end-1] - ♠_old[1:end-1]))`)
-        # dist = cityblock(sub(♠, 1:capT), sub(♠_old, 1:capT)) / capT
-        #
-        # # Now update RHS inplace to be b_hat, then update coefs
-        # b_hat = A_ldiv_B!(qrfact(X, Val{true}), RHS)[1:3]
-        # b_old = scale!(ξ, b_hat) + scale!((1-ξ), b_old)
+        # do VFI to get approximation to value functions/certainty equiv
+        vfs = vfi_from_simulation(m, κ, grid; deg=3, tol=1e-8, maxit=5000)
 
-        dist = mean(abs(1 - ♠./♠_old))
-        @show b_hat = X[1:end-1, :] \ RHS[2:end]
-        b_old = ξ*b_hat + (1-ξ)*b_old
+        #=
+        now construct LHS of regression using the euler equation:
 
-        copy!(♠_old, ♠)
+        ♠' = ♠ * beta_1/beta_2 * g'^(α1-α2) * (c_1'/c_1)^(ρ_1-1) *
+             (c_2'/c_2)^(1-ρ_2) * (U_1'/μ_1)^(α_1 - ρ_1) *(U_2'/μ_2)^(ρ_2 - α_2)
+        =#
+        for t=1:capT-1  # can only go to capT-1 b/c we need c[t+1]
+            # get μ_t based on current state
+            μ1, μ2 = certainty_equiv(m, κ, vfs, ♠[t], zbar[t])
+
+            # get U_{t+1} from simulation
+            U1p, U2p = value_funcs(vfs, ♠[t+1], zbar[t+1])
+
+            # now package up the LHS of the Euler eqn, which is ♠'
+            LHS[t] = (♠[t]*(β1/β2) *
+                      g_all[t+1]^(α1-α2) *
+                      (c1[t+1]/c1[t])^(ρ1 - 1) *
+                      (c2[t+1]/c2[t])^(1 - ρ2) *
+                      (U1p/μ1)^(α1 - ρ1) *
+                      (U2p/μ2)^(ρ2 - α2))
+        end
+
+        # compute dist, update coefs
+        dist = mean(abs(1.0 - ♠./♠_old))
+        κ_hat = X[1:capT-1, :] \ LHS[1:capT-1]
+        κ = (1-ξ)*κ_hat + ξ*κ
+
+
+
+
+        # TODO: need to figure out how to compute euler eqn and run regression
+
 
         it += 1
         if mod(it, 1) == 0
             @printf "Iteration %i, dist %2.5e\n" it dist
-            @show b_old
+            @show κ
         end
     end
 
-    ♠, b_old
+    ♠, κ
 
 end
 
@@ -305,3 +482,37 @@ What I need to do to implement the GSSA algorithm:
     - Use policy rules to simulate
 =#
 end  # module
+
+
+#=
+function foobar(m::BCFL22C, a1::Real, zbar::Real)
+    a2 = 1.0 - a1
+
+    if abs(σ1 - σ2) < 1e-14  # sigmas are the same, used closed form sb
+        y = ((1-ω1)*(1-ω2)/(ω1*ω2))^(1/(σ1-1))*(a1/a2)
+        b2 = zbar / (1 + y)
+
+        # cobb douglass
+        # x = (1-ω1)*(1-ω2)/(ω1*ω2)*(a2/a1)
+        # b2 = zbar*x/(1+x)
+    else
+        error("implement solver for sb")
+    end
+
+    # get rest of allocation
+    b1 = zbar - b2
+    c1 = ((1-ω1)*a1^σ1 + ω1*b1^σ1)^(1/σ1)
+    c2 = ((1-ω2)*b2^σ2 + ω2*a2^σ2)^(1/σ2)
+
+    ♠ = (c2/a2)^(1.0-σ2) * (c1/a1)^(σ1-1.0) * ω2/(1-ω1)
+    ♠2 = (c2/b2)^(1.0-σ2) * (c1/b1)^(σ1-1.0) * (1-ω2)/ω1
+
+    @show ♠, ♠2
+    return ♠
+
+end
+
+function foobar(m::BCFL22C, a1_grid::Vector, zbar_grid::Vector)
+    grid = gridmake(a1_grid, zbar_grid)
+end
+=#
--- Models/TwoAgent/Exchange/scratch.jl
+++ Models/TwoAgent/Exchange/scratch.jl
@@ -1,13 +1,12 @@
 using Bokeh
 
 m = BCFL22C()
-lzbar = simulate_exog(m)
-b_old = [0.00, 1.0, 0.0]  # this is the time additive solution (constnat pareto weights)
-# b_old = [0.05, 0.95, 0.3]
-b0, b1, b2 = b_old
+lzbar, lg = simulate_exog(m)
+κ = [0.05, 0.95, -0.1]
+κ0, κ1, κ2 = κ
 ξ = 0.1
 
-linear_coefs(m, lzbar, b_old)
+linear_coefs(m, lzbar, lg, κ, maxiter=5)
 
 capT = length(lzbar)
 ♠ = ones(capT+1)
@@ -22,6 +21,53 @@ capT = length(lzbar)
     ♠p = b0+ b1*♠t + b2*lzt
     ♠[t+1] = ♠p
 end
-
+t
 showplot(plot(1:capT, ♠[1:capT]))
 showplot(plot(1:capT, lzbar))
+
+
+a1 = 1-1e-15
+t
+
+showplot(plot(1:length(c1)', [c1 c2], legends=["c1", "c2"]))
+
+using StatsBase
+
+@doc quantile
+
+
+foobar(m, 0.4, 1.0)
+
+
+qnwmonomial(2, eye(2), :second)
+
+qnwgh(4, 2)[2]
+
+
+extrema(exp(lzbar))
+1/0.95
+
+
+♠sim = ♠
+zbarsim = exp(lzbar)
+
+showplot(plot(♠sim, zbarsim, "."))
+showplot(plot(eds[:,1], eds[:,2], "."))
+
+x = linspace(0, 2pi)
+y1 = sin(x)
+y2 = cos(x)
+y3 = tan(x)
+
+plot(x, [y1 y2 y3], "rs|bo|g*")
+showplot()
+
+
+tol=1e-8
+deg = 3  # 5.13 seconds
+deg = 5  # 6.48 seconds
+κ = κ_old
+maxit = 5000
+it
+
+x = rand(2, 10)
